98de6d4700271dfe3451785ec7bdd02acf6260ac
Zeican <50028977+Zeican@users.noreply.github.com>
2023-06-03 22:37:09 +0200

    Update MiDaSTestVS.py
    Added VERY SIMPLE video support (mp4, mov). Be aware that the video depth estimation is very slow at the moment, even while using MiDaS Small, as it has to do depth estimation on every frame. Video only exports as mp4 for now.
    
    -TODO- Make it faster!

--- MiDaSTestVS.py
+++ MiDaSTestVS.py
@@ -27,21 +27,11 @@ def load_depth_model(model_name):
         model = torch.hub.load("intel-isl/MiDaS", models[model_name]).eval()
         print("Model found in cache.")
         return model.to(device)
-    except ImportError:
-        print("Error: Missing required package 'torch' or 'torchvision'. Please install them.")
-        sys.exit(1)
-    except KeyError:
-        print("Error: Invalid model name. Please select a valid model.")
-        sys.exit(1)
     except Exception as e:
         print(f"Error loading the model: {e}")
         print("Downloading the model. This may take a moment...")
-        try:
-            model = torch.hub.load("intel-isl/MiDaS", models[model_name], force_reload=True).eval()
-            return model.to(device)
-        except Exception as e:
-            print(f"Error downloading the model: {e}")
-            sys.exit(1)
+        model = torch.hub.load("intel-isl/MiDaS", models[model_name], force_reload=True).eval()
+        return model.to(device)
 
 # Downscale the image
 def downscale_image(image, size):
@@ -70,7 +60,7 @@ def estimate_depth(original_image, downscaled_size, selected_model):
     # Squash the image to a square
     squared_image = rgb_image.resize((downscaled_size[0], int(downscaled_size[0] / aspect_ratio)))
 
-    # Downscale the image
+    # Downscale the image 
     input_image = transforms.ToTensor()(squared_image).unsqueeze(0).to(device)
 
     # Perform depth estimation
@@ -102,8 +92,8 @@ def estimate_depth(original_image, downscaled_size, selected_model):
 # GUI layout
 layout = [
     [sg.Image(key="-IMAGE-", size=(400, 400))],
-    [sg.Button("Select Image"), sg.Text("Model Selection"), sg.Combo(list(models.keys()), size=(12, 1), default_value=default_model, key="-MODEL-")],
-    [sg.Button("Estimate Depth"), sg.Button("Export Depth Map"), sg.Button("Exit")]
+    [sg.Button("Select Image/Video"), sg.Text("Model Selection"), sg.Combo(list(models.keys()), size=(12, 1), default_value=default_model, key="-MODEL-")],
+    [sg.Button("Estimate Depth"), sg.Button("Export Depth Map"), sg.Button("Export as Video"), sg.Button("Exit")]
 ]
 
 # Create the window
@@ -112,6 +102,8 @@ window = sg.Window("Depth Estimation", layout, size=(500, 600))
 # Initialize attributes
 original_image = None
 depth_map_image = None
+video_frames = []
+video_file_path = ""
 
 # Event loop
 while True:
@@ -121,19 +113,43 @@ while True:
     if event == sg.WINDOW_CLOSED or event == "Exit":
         break
 
-    # Handle the "Select Image" button
-    if event == "Select Image":
-        file_path = sg.popup_get_file("Select an image", file_types=(("Image Files", "*.jpg;*.jpeg;*.png"),))
+    # Handle the "Select Image/Video" button
+    if event == "Select Image/Video":
+        file_path = sg.popup_get_file("Select an image or video", file_types=(("Image Files", "*.jpg;*.jpeg;*.png"), ("Video Files", "*.mp4;*.mov")))
         if file_path:
             try:
-                original_image = Image.open(file_path)
-                # Resize the image to fit the maximum size
-                max_width, max_height = 400, 400
-                resized_image = original_image.copy()
-                resized_image.thumbnail((max_width, max_height))
-                window["-IMAGE-"].update(data=ImageTk.PhotoImage(resized_image))
+                if file_path.lower().endswith((".jpg", ".jpeg", ".png")):
+                    # Selected file is an image
+                    original_image = Image.open(file_path)
+                    # Resize the image to fit the maximum size
+                    max_width, max_height = 400, 400
+                    resized_image = original_image.copy()
+                    resized_image.thumbnail((max_width, max_height))
+                    window["-IMAGE-"].update(data=ImageTk.PhotoImage(resized_image))
+                    video_frames = []
+                    video_file_path = ""
+                elif file_path.lower().endswith((".mp4", ".mov")):
+                    # Selected file is a video
+                    video_file_path = file_path
+                    video_frames = []
+                    # Read the video frames
+                    video_capture = cv2.VideoCapture(video_file_path)
+                    while True:
+                        ret, frame = video_capture.read()
+                        if not ret:
+                            break
+                        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                        video_frames.append(Image.fromarray(frame))
+                    video_capture.release()
+                    if len(video_frames) > 0:
+                        original_image = video_frames[0]
+                        # Resize the image to fit the maximum size
+                        max_width, max_height = 400, 400
+                        resized_image = original_image.copy()
+                        resized_image.thumbnail((max_width, max_height))
+                        window["-IMAGE-"].update(data=ImageTk.PhotoImage(resized_image))
             except:
-                sg.popup_error("Failed to open the image file.")
+                sg.popup_error("Failed to open the image or video file.")
 
     # Handle the "Estimate Depth" button
     if event == "Estimate Depth":
@@ -151,8 +167,19 @@ while True:
                 print(f"Aspect ratio: {aspect_ratio}")
                 print(f"Downscaled size: {downscaled_size}")
 
-                # Estimate depth
-                depth_map_image = estimate_depth(original_image, downscaled_size, selected_model)
+                if video_file_path:
+                    # Estimate depth for each frame in the video
+                    estimated_depth_frames = []
+                    for frame in video_frames:
+                        estimated_depth_frame = estimate_depth(frame, downscaled_size, selected_model)
+                        estimated_depth_frames.append(estimated_depth_frame)
+
+                    # Update video frames with depth maps
+                    video_frames = estimated_depth_frames
+                    depth_map_image = video_frames[0]
+                else:
+                    # Estimate depth for the single image
+                    depth_map_image = estimate_depth(original_image, downscaled_size, selected_model)
 
                 # Resize the depth map image to fit the maximum size
                 resized_image = depth_map_image.copy()
@@ -161,8 +188,7 @@ while True:
 
                 # Log: Depth estimation complete
                 print("Depth estimation complete")
-            except Exception as e:
-                print(f"Error: {e}")
+            except:
                 sg.popup_error("Failed to estimate depth.")
 
     # Handle the "Export Depth Map" button
@@ -172,9 +198,33 @@ while True:
                 save_path = sg.popup_get_file("Save depth map", save_as=True, file_types=(("PNG Image", "*.png"),))
                 if save_path:
                     depth_map_image.save(save_path)
-                    sg.popup("Depth map exported successfully.")
+                    sg.popup("Depth map exported successfully!")
             except:
-                sg.popup_error("Failed to export depth map.")
+                sg.popup_error("Failed to export the depth map.")
+
+    # Handle the "Export as Video" button
+    if event == "Export as Video":
+        if video_frames:
+            try:
+                save_path = sg.popup_get_file("Save video file", save_as=True, file_types=(("MP4 Video", "*.mp4"), ("MOV Video", "*.mov")))
+                if save_path:
+                    # Get the original image size
+                    original_size = video_frames[0].size
+                    # Create a video writer object
+                    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+                    video_writer = cv2.VideoWriter(save_path, fourcc, 30, original_size)
+
+                    # Write each frame to the video file
+                    for frame in video_frames:
+                        frame = cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR)
+                        video_writer.write(frame)
+
+                    # Release the video writer
+                    video_writer.release()
+
+                    sg.popup("Video exported successfully!")
+            except:
+                sg.popup_error("Failed to export the video.")
 
 # Close the window
 window.close()
